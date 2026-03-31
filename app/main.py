ddfcf0ee88bb0a83b86d66b0a8b56ded2f7fa175
Abdelali12-codes <jadelmoulaa3@gmail.com>
2026-03-31 23:25:17 +0100

    update to v4

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def health():
 
 @app.route("/")
 def index():
-    return jsonify({"message": "Hello from EC2 deployment v3"})
+    return jsonify({"message": "Hello from EC2 deployment v4"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
